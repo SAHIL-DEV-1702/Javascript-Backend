67bf1ab41249ed151fe8557ba5f976bf5c08b81f
Sahil Patil <patilsahil@172002@gmail.com>
2025-07-29 12:56:01 +0530

    connect to mongo Atlas

---
+++ src/db/index.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "../Constants.js";
+
+
+const connectDB = async () => {
+
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDb connected !! DB HOST: ${connectionInstance.connection.host}`)
+
+    }
+
+    catch (error) {
+
+        console.log("MONGODB CONNECTIONN ERROR ", error)
+        process.exit(1);
+    }
+
+
+}
+
+export default connectDB;
+
+
+
---
+++ src/index.js
@@ -0,0 +1,73 @@
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+
+
+
+
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// THIS IS FIRST APPROACH FOR CONNECT DATABASE BUT IN INDUSTRY THIS IS NOT GOOD OPTION
+
+//  always when connecting any database we need to wrap in promises/try-catch  just beacuse of sometimes databse give an error.
+
+// also it take time to conect thats why we use Async/Await and try catch
+
+
+
+; (async () => {        // IFFE
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+    }
+    catch (error) {
+
+        console.error("error: ", error);
+        throw err
+    }
+})()    // why we write semicolon just before of IIFE(imidiatly invoke function expression ) bcz cleaning ;
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
