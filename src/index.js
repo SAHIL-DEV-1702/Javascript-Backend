6c7c4b0306769ff3996c8b4ae2ef6bac21a698a3
Sahil Patil <patilsahil@172002@gmail.com>
2025-07-29 15:00:27 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -12,10 +12,17 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
 
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running at port ${process.env.PORT}`)
+        })
+    })
 
-
+    .catch((err) => {
+        console.log('MONGO DB CONNECTION FAILED !!!', err);
+    })
 
 
 
---
+++ src/utils/asyncHandeler.js
@@ -0,0 +1,27 @@
+const asyncHandeler = (reqHandler) => {   // THIS is using promise 
+    (req, res, next) => {
+        Promise.resolve(reqHandler(req, res, next)).catch((err) => next(err))
+
+    }
+}
+
+export { asyncHandeler }
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+//const asyncHandeler = (fun) => async() => {}
+
+// const asyncHandeler = (fn) => async (req, res, next) => {   // this is using try catch code
+//     try {
+//         await fn(req,res,next)
+
+//     }
+//     catch (error) {
+//         res.status(err.code || 400).json({
+//             sucess: false,
+//             message:err.message
+//         })
+//     }
+
+// }
+
